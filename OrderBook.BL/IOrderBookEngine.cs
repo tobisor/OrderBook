e29769fc705a8eeb5f571e354446f8ba24f7f2f2
tobisor <or.tobis@gmail.com>
2021-08-26 17:06:03 +0300

    Add files via upload

---
+++ OrderBook.BL/IOrderBookEngine.cs
@@ -0,0 +1,16 @@
+﻿using OrderBook.BL.Models;
+using System.Collections.Generic;
+
+namespace OrderBook.BL
+{
+    public interface IOrderBookEngine
+    {
+        void Start();
+
+        void PushOrders(IEnumerable<OrderBookEntry> orders);
+
+        IList<OrderBookEntry> GetBids();
+
+        IList<OrderBookEntry> GetAsks();
+    }
+}
---
+++ OrderBook.BL/IOrdersExecutor.cs
@@ -0,0 +1,9 @@
+﻿using OrderBook.BL.Models;
+
+namespace OrderBook.BL
+{
+    public interface IOrdersExecutor
+    {
+        bool ExecuteOrders(OrderBookEntry buyOrder, OrderBookEntry sellOrder);
+    }
+}
